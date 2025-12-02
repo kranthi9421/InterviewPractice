3e2abf9af921fb009a394b64b1bfad14eeb247c4
kranthi kumar <kranthi.007007@gmail.com>
2025-12-02 15:52:26 +0530

    Update ModalDemo.tsx

--- src/Modal/ModalDemo.tsx
+++ src/Modal/ModalDemo.tsx
@@ -8,7 +8,7 @@ export const ModalDemo = () => {
         <div>
           <h1>Hayu</h1>
           <button onClick={() => setModalOpen(true)}>Show modal</button>
-          <Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)}>
+          <Modal isModalOpen={isModalOpen} onClose={() => setModalOpen(false)}>
             <h1>This is modal</h1>
           </Modal>
         </div>
