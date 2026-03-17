6f847b13709f87cf9720c11b61b6d5926228e806
kranthi kumar <kranthi.007007@gmail.com>
2026-03-17 12:10:38 +0530

    Update Parallel.tsx

--- src/ParallelDataFetch/Parallel.tsx
+++ src/ParallelDataFetch/Parallel.tsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from "react";
 
-/* ---------- Types ---------- */
-
 type Post = {
   id: number;
   title: string;
@@ -12,77 +10,51 @@ type User = {
   name: string;
 };
 
-/* ---------- Component ---------- */
-
 export const Parallel = () => {
   const [posts, setPosts] = useState<Post[]>([]);
   const [users, setUsers] = useState<User[]>([]);
-  const [postError, setPostError] = useState<string>("");
-  const [userError, setUserError] = useState<string>("");
-
-  const getPosts = async ()=>{
-    try {
-      const res = await fetch(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
-
-      if (!res.ok) throw new Error("Failed to fetch posts");
-
-      setPostError("");
-      return await res.json();
-    } catch {
-      setPostError("Error fetching posts");
-      return [];
-    }
-  };
-
-  const getUsers = async ()=>{
-    try {
-      const res = await fetch(
-        "https://jsonplaceholder.typicode.com/users"
-      );
-
-      if (!res.ok) throw new Error("Failed to fetch users");
-
-      setUserError("");
-      return await res.json();
-    } catch {
-      setUserError("Error fetching users");
-      return [];
-    }
-  };
-
-  const mainData = async () => {
-    const [postsData, usersData] = await Promise.all([
-      getPosts(),
-      getUsers(),
-    ]);
-
-    setPosts(postsData);
-    setUsers(usersData);
-  };
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    mainData();
+    const fetchData = async () => {
+      try {
+        const [postsRes, usersRes] = await Promise.all([
+          fetch("https://jsonplaceholder.typicode.com/posts"),
+          fetch("https://jsonplaceholder.typicode.com/users"),
+        ]);
+
+        if (!postsRes.ok || !usersRes.ok) {
+          throw new Error("Something went wrong");
+        }
+
+        const [postsData, usersData] = await Promise.all([
+          postsRes.json(),
+          usersRes.json(),
+        ]);
+
+        setPosts(postsData);
+        setUsers(usersData);
+      } catch (err) {
+        setError("Error fetching data");
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
     <div className="flex justify-around">
       <div>
-        {postError
-          ? <p>{postError}</p>
-          : posts.map(post => (
-              <p key={post.id}>{post.title}</p>
-            ))
+        {error
+          ? <p>{error}</p>
+          : posts.map(post => <p key={post.id}>{post.title}</p>)
         }
       </div>
 
       <div>
-        {userError
-          ? <p>{userError}</p>
-          : users.map(user => (
-              <p key={user.id}>{user.name}</p>
-            ))
+        {error
+          ? <p>{error}</p>
+          : users.map(user => <p key={user.id}>{user.name}</p>)
         }
       </div>
     </div>
