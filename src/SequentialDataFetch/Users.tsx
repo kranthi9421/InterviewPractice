89afee217ac32442317ea8e59c3a22bfeaa830b5
kranthi kumar <kranthi.007007@gmail.com>
2026-08-29 18:41:17 +0530

    Update Users.tsx

--- src/SequentialDataFetch/Users.tsx
+++ src/SequentialDataFetch/Users.tsx
@@ -1,36 +1,36 @@
 import { useEffect, useState } from "react";
 
-/* ---------- Types ---------- */
-
 type User = {
   name: string;
 };
 
-/* ---------- Component ---------- */
-
-const Users = ({ userId }: {userId: number}) => {
+const Users = ({ userId }: { userId: number }) => {
   const [user, setUser] = useState<User | null>(null);
 
-  const getData = async () => {
-    try {
-      const res = await fetch(
-        `https://jsonplaceholder.typicode.com/users/${userId}`
-      );
-      const data: User = await res.json();
-      setUser(data);
-    } catch (error) {
-      console.error("Error fetching user:", error);
-    }
-  };
-
   useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await fetch(
+          `https://jsonplaceholder.typicode.com/users/${userId}`
+        );
+
+        const data: User = await res.json();
+
+        setUser(data);
+      } catch (error) {
+        console.error("Error fetching user:", error);
+      }
+    };
+
     getData();
   }, [userId]);
 
-  if (!user) return <p>Loading user...</p>;
+  if (!user) {
+    return <p>Loading user...</p>;
+  }
 
   return (
-    <span className="text-red-950 text-2xl">
+    <span>
       UserName: {user.name}
     </span>
   );
